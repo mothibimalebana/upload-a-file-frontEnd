2243b5cf99d13536f6a7ead10fb1e629af32315f
mothibimalebana <mothibi.mm12@gmail.com>
2025-10-21 18:36:24 +0200

    animation: the outlet eases into the page smoothly

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useParams } from 'react-router-dom';
 import './App.css'
 import Navbar from './components/Navbar';
 import { useState } from 'react';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 
 function App() {
-  const [selected, useSelected] = useState('Home');
+  const [selectedPage, useSelectedPage] = useState(window.location.pathname.slice(1));
+
+  useGSAP(() => {
+    gsap.from(".app-content",{duration: 1, y: '100%', ease: 'power4.out'})
+  }, {dependencies:[selectedPage]})
+
 
   return (
     <>
-    <div id='app' className="app grid md:grid-cols-[250px_minmax(900px,_auto)] bg-[#fff]">
-      <Navbar page={useSelected} currPage={selected}/>
-      <div className="app-content">
-        <Outlet/>
+    <div id='app' className="app grid overflow-y-hidden overflow-x-hidden md:grid-cols-[250px_minmax(900px,_auto)] bg-[#fff]">
+      <Navbar page={useSelectedPage} currPage={selectedPage}/>
+      <div className="app-content w-full">
+        <Outlet id='outlet'/>
       </div>
     </div>
     </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react"
 import { Link } from "react-router-dom"
 
+
 const Navbar = ({page, currPage}) => {
+
 
     const topNavLinks = [
         /** home **/
