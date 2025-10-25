78187cfbbaab77c365aaf88933fe22e89e99ee3d
mothibimalebana <mothibi.mm12@gmail.com>
2025-10-25 14:18:12 +0200

    chore: created File component for the Home element

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,25 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom"
 
+const Files = ({files}) => {
 
-const Home = () => {
+    return(
+        <div className="files">
+            {files != [] ? <p> nothing to see here, have  you tried uploading something?</p> :
+                <div className="file">
+                    {files.map((file) => console.log(file))}
+                </div>
+            }
+        </div>
+    )
+}
+const Home = ({files}) => {
+
     return(
         <>
             <div className="container">
                 <p className="text-start" >Recently</p>
+                <Files files={files}/>
             </div>
         </>
     )
